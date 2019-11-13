66c90e4b71b809f10f17416182e1ed7d570af293
petarcurcin <petar.curcin97@gmail.com>
2019-11-13 23:55:05 +0100

    Added profile page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import {LoginComponent} from './login/login.component';
 import {LoginService} from './login/login.service';
 import {RegistrationRequestComponent} from './registration-request/registrationRequest.component';
 import {RegistrationRequestService} from './registration-request/registrationRequest.service';
+import {MedicalStaffProfileService} from './medical-staff-profile/medicalStaffProfile.service';
+import {MedicalStaffProfileComponent} from './medical-staff-profile/medicalStaffProfile.component';
 import {MoreInfoRegisterRequestComponent} from './more-info/more-info-register-request/moreInfoRegisterRequest.component';
 import {MoreInfoRegisterRequestService} from './more-info/more-info-register-request/moreInfoRegisterRequest.service';
 
@@ -24,6 +26,8 @@ import {MoreInfoRegisterRequestService} from './more-info/more-info-register-req
     LoginComponent,
     RegistrationRequestComponent,
     MoreInfoRegisterRequestComponent,
+    RegistrationRequestComponent,
+    MedicalStaffProfileComponent
   ],
   imports: [
     BrowserModule,
@@ -32,7 +36,8 @@ import {MoreInfoRegisterRequestService} from './more-info/more-info-register-req
     HttpClientModule,
     FormsModule
   ],
-  providers: [RegistrationService, MedicamentService, LoginService, RegistrationRequestService, MoreInfoRegisterRequestService],
+  providers: [RegistrationService, MedicamentService, LoginService, RegistrationRequestService, MedicalStaffProfileService,
+     MoreInfoRegisterRequestService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
