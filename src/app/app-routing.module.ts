5832466ea9112e4feee9ffe09e7ace4bfa1050d8
petarcurcin <petar.curcin97@gmail.com>
2019-11-14 15:23:52 +0100

    Add root for support edit medical staff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import {LoginComponent} from './login/login.component';
 import {RegistrationRequestComponent} from './registration-request/registrationRequest.component';
 import {MoreInfoRegisterRequestComponent} from './more-info/more-info-register-request/moreInfoRegisterRequest.component';
 import {MedicalStaffProfileComponent} from './medical-staff-profile/medicalStaffProfile.component';
+import {EditMedicalStaffComponent} from './edit-medical-staff/editMedicalStaff.component';
 
 
 const routes: Routes = [{path: 'register', component: RegistrationComponent},
@@ -13,7 +14,7 @@ const routes: Routes = [{path: 'register', component: RegistrationComponent},
                         {path: 'login', component: LoginComponent},
                         {path: 'registrationRequests', component: RegistrationRequestComponent},
                         {path: 'medicalStaffProfile', component: MedicalStaffProfileComponent},
-                        {path: 'registrationRequests', component: RegistrationRequestComponent},
+                        {path: 'editMedicalStaff', component: EditMedicalStaffComponent},
                         {path: 'registrationRequest/:id', component: MoreInfoRegisterRequestComponent}];
 
 @NgModule({
