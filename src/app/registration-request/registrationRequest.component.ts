0a20b9e3636ec8776301fd0c6377119aae916624
petarcurcin <petar.curcin97@gmail.com>
2019-11-20 20:30:07 +0100

    Reject register requests

--- src/app/registration-request/registrationRequest.component.ts
+++ src/app/registration-request/registrationRequest.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {RegistrationRequest} from '../model/registrationRequest';
 import {RegistrationRequestService} from './registrationRequest.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 
 @Component({
@@ -12,12 +13,22 @@ export class RegistrationRequestComponent implements OnInit {
 
   requests: RegistrationRequest[] = [];
 
-  constructor(private registrationRequestService: RegistrationRequestService) {
+  constructor(private registrationRequestService: RegistrationRequestService , private router: Router) {
   }
 
   ngOnInit(): void {
     this.registrationRequestService.getAll().subscribe(data => {
       this.requests = data;
     });
+  }
+
+
+  rejectRequest(id: number) {
+    this.registrationRequestService.removeRequest(id).subscribe(result => this.gotoRegistrationRequest());
+    console.log(id);
+  }
+
+  private gotoRegistrationRequest() {
+    this.router.navigate(['/registrationRequests']);
   }
 }
