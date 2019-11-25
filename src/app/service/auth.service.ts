d8a214cf03676a1ee3680a64d7a6e5b47b1a307c
petarcurcin <petar.curcin97@gmail.com>
2019-11-25 17:03:28 +0100

    Reject register requests

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -27,7 +27,7 @@ export class AuthService {
     });
     // const body = `username=${user.username}&password=${user.password}`;
     const body = {
-      'email' : user.email,
+      'username' : user.email,
       'password' : user.password
     };
     return this.apiService.post(this.config.login_url, JSON.stringify(body), loginHeaders)
