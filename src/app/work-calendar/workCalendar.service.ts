fb72bcf4924a58780eb1da24307658c988fc2746
jelenas97 <jekibp@gmail.com>
2020-01-25 04:18:17 +0100

    Added real exams in work calendar

--- src/app/work-calendar/workCalendar.service.ts
+++ src/app/work-calendar/workCalendar.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {MedicalExamination} from '../model/medicalExamination';
 
 @Injectable()
 export class WorkCalendarService {
@@ -7,4 +8,7 @@ export class WorkCalendarService {
   constructor(private http: HttpClient) {
   }
 
+  getAllExams(id: string) {
+    return this.http.get<MedicalExamination[]>('http://localhost:8080/medicalExaminations/doctor/' + id);
+  }
 }
