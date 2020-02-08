20d7a092077642ad5b6bad14dc86bcc2b68ed98d
jelenas97 <jekibp@gmail.com>
2020-02-08 14:19:46 +0100

    code fix

--- src/app/patient-home-page/profile-page/profile-page.component.ts
+++ src/app/patient-home-page/profile-page/profile-page.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {User} from '../../model/user';
 import {PatientHomePageService} from '../patientHomePage.service';
 import {ActivatedRoute} from '@angular/router';
+import {UserService} from '../../service/user.service';
 
 @Component({
   selector: 'app-profile-page',
@@ -12,11 +13,13 @@ export class ProfilePageComponent implements OnInit {
 
   user: User;
 
-  constructor(private patientHomePageService: PatientHomePageService, private activatedRoute: ActivatedRoute) {
+  constructor(private patientHomePageService: PatientHomePageService, private userService: UserService) {
   }
 
   ngOnInit() {
-    this.patientHomePageService.getById(1).subscribe(data => {
+    this.userService.getMyInfo();
+    this.user = this.userService.currentUser;
+    this.patientHomePageService.getById(+this.user.id).subscribe(data => {
       this.user = data;
     });
   }
