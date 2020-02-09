11733bc6b734f51ae03cba7247cfafc4a8de9d52
petarcurcin <petar.curcin97@gmail.com>
2020-02-09 02:07:20 +0100

    implemented transaction

--- src/app/clinic-administrator-home-page/vacation-and-absence-view/vacationAndAbsenceView.component.ts
+++ src/app/clinic-administrator-home-page/vacation-and-absence-view/vacationAndAbsenceView.component.ts
@@ -6,6 +6,7 @@ import {UserService} from '../../service/user.service';
 import {User} from '../../model/user';
 import {ModalDismissReasons, NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {NotifierService} from 'angular-notifier';
 
 @Component({
   templateUrl: 'vacationAndAbsenceView.component.html',
@@ -20,9 +21,13 @@ export class VacationAndAbsenceViewComponent implements OnInit {
   closeResult: string;
   reason: string;
   requestId: number;
+  notifier: NotifierService;
+
 
   constructor(private vacationAndAbsenceViewService: VacationAndAbsenceViewService, private route: ActivatedRoute,
-              private router: Router, private userService: UserService, private modalService: NgbModal) {
+              private router: Router, private userService: UserService, private modalService: NgbModal,
+              private notifierService: NotifierService) {
+    this.notifier = notifierService;
   }
 
   ngOnInit(): void {
@@ -50,7 +55,30 @@ export class VacationAndAbsenceViewComponent implements OnInit {
   }
 
   sendApproveMailtoUser(requestVacation: Vacation, id: number): void {
-    this.vacationAndAbsenceViewService.sendApproveMail(requestVacation, id).subscribe(result => this.ngOnInit());
+
+    if (window.location.href.indexOf('vacationRequests') > -1) {
+      this.vacationAndAbsenceViewService.getAllVacationRequests(this.user.id).subscribe(data => {
+        this.requests = data;
+        if (this.requests.some((item) => item.id === id)) {
+          this.vacationAndAbsenceViewService.sendApproveMail(requestVacation, id).subscribe(result => this.ngOnInit());
+          this.showNotification('success', 'You have successfully approved request.');
+
+        } else {
+          this.showNotification('warning', 'Request is handled in meantime.');
+        }
+      });
+    } else {
+      this.vacationAndAbsenceViewService.getAllAbsenceRequests(this.user.id).subscribe(data => {
+        this.requests = data;
+        if (this.requests.some((item) => item.id === id)) {
+          this.vacationAndAbsenceViewService.sendApproveMail(requestVacation, id).subscribe(result => this.ngOnInit());
+          this.showNotification('success', 'You have successfully approved request.');
+
+        } else {
+          this.showNotification('warning', 'Request is handled in meantime.');
+        }
+      });
+    }
   }
 
 
@@ -65,8 +93,28 @@ export class VacationAndAbsenceViewComponent implements OnInit {
   }
 
   onSubmit(mymodal) {
-     this.vacationAndAbsenceViewService.sendRejectMail(this.reason, this.requestId).subscribe(result => this.ngOnInit());
-     this.modalService.dismissAll();
+    if (window.location.href.indexOf('vacationRequests') > -1) {
+      this.vacationAndAbsenceViewService.getAllVacationRequests(this.user.id).subscribe(data => {
+        this.requests = data;
+        if (this.requests.some((item) => item.id === this.requestId)) {
+          this.vacationAndAbsenceViewService.sendRejectMail(this.reason, this.requestId).subscribe(result => this.ngOnInit());
+          this.showNotification('success', 'You have successfully rejected request.');
+        } else {
+          this.showNotification('warning', 'Request is handled in meantime.');
+        }
+      });
+    } else {
+      this.vacationAndAbsenceViewService.getAllAbsenceRequests(this.user.id).subscribe(data => {
+        this.requests = data;
+        if (this.requests.some((item) => item.id === this.requestId)) {
+          this.vacationAndAbsenceViewService.sendRejectMail(this.reason, this.requestId).subscribe(result => this.ngOnInit());
+          this.showNotification('success', 'You have successfully rejected request.');
+        } else {
+          this.showNotification('warning', 'Request is handled in meantime.');
+        }
+      });
+    }
+    this.modalService.dismissAll();
   }
 
   openModal(mymodal, request: Vacation) {
@@ -76,5 +124,9 @@ export class VacationAndAbsenceViewComponent implements OnInit {
     }, (reason) => {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
     });
+  }
+
+  public showNotification(type: string, message: string): void {
+    this.notifier.notify(type, message);
   }
 }
